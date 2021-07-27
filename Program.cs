9e9f2096b2ba604957c57dca460797fc61c4fc05
Flyppo33 <81187648+dim-mar-dev@users.noreply.github.com>
2021-07-27 10:32:49 +0200

    Initial commit - moving project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsGitHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CsGitHello Hello World!");
+        }
+    }
+}
